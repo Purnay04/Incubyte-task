eb716771aa5b66288b1b74dc4a201d9a879b8fd6
Purnay Barge <bargeamanpurnay087@gmail.com>
2024-11-06 01:06:05 +0530

    Support different delimiters

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,20 +3,44 @@ package org.example;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public class Calculator {
+
+    public final static String DEFAULT_DELIMITER = ",";
+    public final static String NEW_LINE = "\n";
     static int add(String numbers) {
         if(StringUtils.isEmpty(numbers)) {
             return 0;
-        } else if((numbers = preprocess(numbers)).contains(",")) {
-            String[] numbersArray = numbers.split(",");
-            return Arrays.stream(numbersArray).filter(StringUtils::isNotEmpty).mapToInt(Integer::parseInt).sum();
+        } else if(numbers.startsWith("//")) {
+            Optional<Character> delimiterChar = getDelimiter(numbers);
+            if(delimiterChar.isPresent()) {
+                String delimiter =  String.valueOf(delimiterChar.get());
+                numbers = preprocess(numbers.substring(4), delimiter);  // offset 4 is to skip indexes of // & delimiterChar \n characters
+                return sumOfStringArr(numbers.split(delimiter));
+            }
+            numbers = preprocess(numbers.substring(3), DEFAULT_DELIMITER);  // offset 3 is to skip indexes of // & \n characters
+            return sumOfStringArr(numbers.split(DEFAULT_DELIMITER));
+        } else if((numbers = preprocess(numbers, DEFAULT_DELIMITER)).contains(DEFAULT_DELIMITER)) {
+            return sumOfStringArr(numbers.split(DEFAULT_DELIMITER));
         } else {
             return Integer.parseInt(numbers);
         }
     }
 
-    static String preprocess(String numbers) {
-        return numbers.replaceAll("\n", ",");
+    static String preprocess(String numbers, String delimiter) {
+        return numbers.replaceAll(NEW_LINE, delimiter);
+    }
+
+    static Optional<Character> getDelimiter(String numbers) {
+        if(StringUtils.isNotEmpty(numbers) && numbers.length() > 3) {
+            char delimiter = numbers.charAt(2);
+            return delimiter == '\n' ? Optional.empty() : Optional.of(delimiter);
+        }
+        return Optional.empty();
+    }
+
+    static int sumOfStringArr(String[] numberArray) {
+        return Arrays.stream(numberArray).filter(StringUtils::isNotEmpty).mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -4,7 +4,11 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 class CalculatorTest {
+    public final static String NEW_LINE = "\n";
+    public final static String DEFAULT_DELIMITER = ",";
 
     // Test case naming convention is like <TEST_METHOD>_<INPUT>_<EXPECTED_OUTPUT>
     @Test
@@ -40,8 +44,49 @@ class CalculatorTest {
     }
 
     @Test
-    void preprocess_numbersWithCommaSeparatedStringAndNewLineChar_replacedResult() {
-        Assertions.assertEquals("1,2,3,4", Calculator.preprocess("1\n2,3,4"));
-        Assertions.assertEquals("1,,2,3,4", Calculator.preprocess("1\n,2,3,4"));
+    void add_numbersWithCustomDelimiter_addedResult() {
+        Assertions.assertEquals(10, Calculator.add("//;\n10"));
+        Assertions.assertEquals(3, Calculator.add("//;\n1;2"));
+        Assertions.assertEquals(7, Calculator.add("//,\n1,4,2"));
+        Assertions.assertEquals(50, Calculator.add("//'\n1'2'3'4'25'15"));
     }
+
+    @Test
+    void add_numbersWithCustomDelimiterAndNewLineChar_addedResult() {
+        Assertions.assertEquals(10, Calculator.add("//;\n\n10"));
+        Assertions.assertEquals(3, Calculator.add("//;\n1\n;2"));
+        Assertions.assertEquals(7, Calculator.add("//,\n1,4\n,2"));
+        Assertions.assertEquals(50, Calculator.add("//'\n1'2\n3'4\n25'15"));
+        Assertions.assertEquals(50, Calculator.add("//\n1,2\n3,4\n25,15"));
+    }
+
+    @Test
+    void preprocess_numbersWithCommaSeparatedStringAndNewLineChar_replacedResult() {
+        Assertions.assertEquals("1,2,3,4", Calculator.preprocess("1\n2,3,4", DEFAULT_DELIMITER));
+        Assertions.assertEquals("1,,2,3,4", Calculator.preprocess("1\n,2,3,4", DEFAULT_DELIMITER));
+    }
+
+    @Test
+    void getDelimiter_correctFormattedString_actualDelimiter() {
+        Assertions.assertEquals(Optional.of(';'), Calculator.getDelimiter("//;\n\n10"));
+        Assertions.assertEquals(Optional.of(','), Calculator.getDelimiter("//,\n1,4\n,2"));
+    }
+
+    @Test
+    void getDelimiter_withoutDelimiter_empty() {
+        Assertions.assertEquals(Optional.empty(), Calculator.getDelimiter("//\n\n10"));
+    }
+
+    @Test
+    void sumOfStringArr_emptyStringArr_Zero() {
+        Assertions.assertEquals(0, Calculator.sumOfStringArr(new String[]{}));
+    }
+
+    @Test
+    void sumOfStringArr_numberString_sumOfNumbers() {
+        Assertions.assertEquals(6, Calculator.sumOfStringArr(new String[]{"1", "2", "3"}));
+        Assertions.assertEquals(7, Calculator.sumOfStringArr(new String[]{"1", "4", "2"}));
+        Assertions.assertEquals(22, Calculator.sumOfStringArr(new String[]{"10", "12"}));
+    }
+
 }
